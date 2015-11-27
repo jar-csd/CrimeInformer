46bbcf41ee1695a55c9ae94c06299389ad88d394
jar <jar_khunnon@hotmail.co.th>
2015-11-27 11:04:52 +0700

    start

---
+++ app/src/main/java/csd/jar/crimeinformer/MainActivity.java
@@ -0,0 +1,24 @@
+package csd.jar.crimeinformer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }  //onCreate
+
+    public void clickLogin(View view) {
+
+    }
+
+    public void clickRegister(View view) {
+        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+    }
+
+}   //Main  Class
