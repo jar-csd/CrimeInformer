c1722aba8a132673afa1e77fdd3224501d68866a
jar <jar_khunnon@hotmail.co.th>
2015-12-25 11:14:29 +0700

    Chack Space Finish

--- app/src/main/java/csd/jar/crimeinformer/LoginActivity.java
+++ app/src/main/java/csd/jar/crimeinformer/LoginActivity.java
@@ -2,15 +2,47 @@ package csd.jar.crimeinformer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Bind Widget
+        bindWidget();
     } // Main Method
 
+    public void clickLogin(View view) {
+
+        //Check Space
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("") ) {
+
+            //Have Space
+            MyAlertDialog objMyAlertDialog = new MyAlertDialog();
+            objMyAlertDialog.nagativeDialog(LoginActivity.this, "Have Space", "Please Fill All Every Blank");
+
+        } else {
+
+        }
+
+    }// ClickLogin
+
+    private void bindWidget() {
+        userEditText = (EditText) findViewById(R.id.editText6);
+        passwordEditText = (EditText) findViewById(R.id.editText7);
+
+    }
 
 
 } // Main Class
