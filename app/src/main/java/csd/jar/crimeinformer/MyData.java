61f17d51ccfeb5c2c83be81618e0b850b7ca567f
jar <jar_khunnon@hotmail.co.th>
2016-01-29 15:05:08 +0700

    Finish 29 Jan16

---
+++ app/src/main/java/csd/jar/crimeinformer/MyData.java
@@ -0,0 +1,14 @@
+package csd.jar.crimeinformer;
+
+/**
+ * Created by Administrator on 29/1/2559.
+ */
+public class MyData {
+
+    //Explicit
+    public String[] mainHeadStrings = {"MainHead1", "MainHead2", "MainHead3"};
+    public String[][] subHeadStrings = {{"subHead11","subHead12","subHead13","subHead14","subHead15",},
+            {"subHead21","subHead22","subHead23","subHead24",},
+            {"subHead31","subHead32","subHead33","subHead34",}};
+
+}//Main Class
